dfedc8ab06239822886f32bd3e6b13ad4c793c5c
rmxhaha <code000123@gmail.com>
2016-09-27 14:21:50 +0700

    test code in main

--- main.cpp
+++ main.cpp
@@ -12,13 +12,19 @@ int main(){
   RoomVector rooms;
   rooms.emplace_back(new Classroom("7606",8,16));
   rooms.emplace_back(new Classroom("7602",8,16));
-  CourseVector courses;
 
+  CourseVector courses;
   courses.emplace_back(new Course("AI",2, 7, 15));
   courses.emplace_back(new Course("TBFO",3, 7, 12));
 
-//  course->set_schedule(Schedule(room,Day::Monday,8,9));
-//  course->get_schedule();
+  shared_ptr<Classroom> room(new Classroom("7606",8,16));
+  Course course("AI",2,7,15);
+  course.set_schedule(Schedule(room,Day::Monday,8,10));
+  course.get_schedule();
+
+  Course course2(course);
+
+  cout << course2.get_schedule().start_time << endl;
 
 //  Schedule s1(room,Day::Monday,8,9);
 //  Schedule s2(room2,Day::Monday,7,9);
