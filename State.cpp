58d8503ac2985c276201bb29a4fcd0bafa38df11
Michael <miciltjandra@gmail.com>
2016-09-29 22:16:16 +0700

    Add day and room constraint to random, uses Mersenne Twister random

--- State.cpp
+++ State.cpp
@@ -1,6 +1,9 @@
 #include "State.hpp"
 #include <iostream>
+#include <algorithm>
 #include <stdlib.h>
+#include <chrono>
+#include <random>
 
 // do deep copy for courses and shallow copy for classroom
 State::State(const RoomVector& r, const CourseVector& c) : rooms(r){
@@ -13,27 +16,56 @@ State::State(const State& s) : State(s.rooms,s.courses){
 }
 
 void State::init_random_schedule(){
-  std::default_random_engine generator;
+  //std::default_random_engine generator;
+  
+  //Mersenne Twister Random generator
+  typedef std::mt19937 MyRNG;
+  uint32_t seed_val = std::chrono::system_clock::now().time_since_epoch().count();
+  MyRNG generator;
+  generator.seed(seed_val);
 
   std::uniform_int_distribution<int> day_dist(0,4);
 
   for( auto& it : courses ){
-    // random location
+    // random location & day
     auto& crooms = it->get_possible_classroom();
     std::uniform_int_distribution<int> room_idx_dist(0,crooms.size()-1);
 
-    shared_ptr<Classroom> room = crooms[ room_idx_dist(generator) ];
+    shared_ptr<Classroom> room;
 
-    // random time
+    vector<Day> room_possible_day;
+	vector<Day> course_possible_day;
+	vector<Day> possible_day;
+	while (possible_day.empty()) { //if no possible_day for selected room and course
+		room = crooms[ room_idx_dist(generator) ];    
+		room_possible_day = room->get_possible_day();
+		course_possible_day = it->get_possible_day();
+		//intersect to find possible day for course it in room
+		std::set_intersection(room_possible_day.begin(),room_possible_day.end(),course_possible_day.begin(),course_possible_day.end(),back_inserter(possible_day));
+	}
+	std::cout << "Room : " << room->name << " course : " << it->name << "\n";
+	std::cout << "Possible day : ";
+	for (auto it : possible_day) {
+		cout << static_cast<int>(it) << " ";
+	}
+	cout << "\n";
+	
+    Day d = static_cast<Day>( day_dist(generator) );
+	//search for a day that is possible
+	while( !(std::find(possible_day.begin(), possible_day.end(), d) != possible_day.end())) {
+		d = static_cast<Day>( day_dist(generator) );
+	}
+	
+	
+	// random time
     const int ot = std::max( room->open_time, it->open_time );
     const int ct = std::min( room->close_time, it->close_time );
     std::uniform_int_distribution<int> start_time_dist(ot,ct-it->duration);
-
-    Day d = static_cast<Day>( day_dist(generator) );
     int st = start_time_dist(generator);
     int et = st + it->duration;
 
     it->set_schedule(Schedule(room,d,st,et));
+	std::cout << it->get_schedule().room->name << "_" << static_cast<int>(it->get_schedule().day) << "_" << it->get_schedule().start_time << "_" << it->get_schedule().end_time << "\n";
   }
   
 }
@@ -58,11 +90,61 @@ State State::mutate(){
   State s = *this;
   // TODO : alter state randomly
   
+  //Mersenne Twister Random generator
+  typedef std::mt19937 MyRNG;
+  uint32_t seed_val = std::chrono::system_clock::now().time_since_epoch().count();
+  MyRNG generator;
+  generator.seed(seed_val);
+  
+  std::uniform_int_distribution<int> day_dist(0,4);
+  std::uniform_int_distribution<int> course_dist(0,(s.courses.size()-1));
+  
   //random course
-  shared_ptr<Course> altered_course = s.courses[rand()%s.courses.size()];
+  shared_ptr<Course> altered_course = s.courses[course_dist(generator)];
   //cout << "got course" << altered_course->name << "\n";
   
-  // random location
+  // random location & day
+  auto& crooms = altered_course->get_possible_classroom();
+  std::uniform_int_distribution<int> room_idx_dist(0,crooms.size()-1);
+
+  shared_ptr<Classroom> room;
+
+  vector<Day> room_possible_day;
+  vector<Day> course_possible_day;
+  vector<Day> possible_day;
+  while (possible_day.empty()) { //if no possible_day for selected room and course
+    room = crooms[ room_idx_dist(generator) ];    
+	room_possible_day = room->get_possible_day();
+	course_possible_day = altered_course->get_possible_day();
+	//intersect to find possible day for course altered_course in room
+	std::set_intersection(room_possible_day.begin(),room_possible_day.end(),course_possible_day.begin(),course_possible_day.end(),back_inserter(possible_day));
+  }
+  std::cout << "Room : " << room->name << " course : " << altered_course->name << "\n";
+  std::cout << "Possible day : ";
+  for (auto it : possible_day) {
+	cout << static_cast<int>(it) << " ";
+  }
+  cout << "\n";
+
+  Day d = static_cast<Day>( day_dist(generator) );
+  //search for a day that is possible
+  while( !(std::find(possible_day.begin(), possible_day.end(), d) != possible_day.end())) {
+	d = static_cast<Day>( day_dist(generator) );
+  }
+
+
+  // random time
+  const int ot = std::max( room->open_time, altered_course->open_time );
+  const int ct = std::min( room->close_time, altered_course->close_time );
+  std::uniform_int_distribution<int> start_time_dist(ot,ct-altered_course->duration);
+  int st = start_time_dist(generator);
+  int et = st + altered_course->duration;
+
+  altered_course->set_schedule(Schedule(room,d,st,et));
+  for (auto& it : s.courses) {
+    std::cout << it->get_schedule().room->name << "_" << static_cast<int>(it->get_schedule().day) << "_" << it->get_schedule().start_time << "_" << it->get_schedule().end_time << "\n";
+  }
+  /*// random location
   auto& crooms = altered_course->get_possible_classroom();
   shared_ptr<Classroom> room  =crooms[rand()%crooms.size()];
   //cout << "got class" << room->name << "\n";
@@ -79,17 +161,17 @@ State State::mutate(){
   int st = (rand()%(1+ct-ot-(altered_course->duration)))+ot;
   //cout << "st" << st <<"\n";
   int et = st + altered_course->duration;
-  /*cout << "et\n";
-  cout << "day" << static_cast<int>(d) << " st" << st << " et" << et << "\n"; */
+  //cout << "et\n";
+  //cout << "day" << static_cast<int>(d) << " st" << st << " et" << et << "\n"; 
   
   altered_course->set_schedule(Schedule(room,d,st,et));
   
   //cout << "altered\n";
   
   for (auto& it : s.courses) {
-    std::cout << it->get_schedule().room->name << "_" << static_cast<int>(it->get_schedule().day) << "_" << it->get_schedule().start_time << "_" << it->get_schedule().end_time << "  ";
+    //std::cout << it->get_schedule().room->name << "_" << static_cast<int>(it->get_schedule().day) << "_" << it->get_schedule().start_time << "_" << it->get_schedule().end_time << "  ";
   }
-  std::cout << "\n";
+  //std::cout << "\n";*/
   return s;
 }
 
