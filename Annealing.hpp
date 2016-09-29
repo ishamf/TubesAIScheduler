0dafec228e9060ca40d30f28f1a3e7c9a9711016
Michael <miciltjandra@gmail.com>
2016-09-29 13:15:04 +0700

    Add simulated annealing

---
+++ Annealing.hpp
@@ -0,0 +1,19 @@
+#ifndef ANNEALING_HPP
+#define ANNEALING_HPP
+
+#include "State.hpp"
+#include <cmath>
+
+class Annealing {
+public:
+  Annealing(State init, const double t, const double c);
+  double countAcceptanceRate(const int currentscore, const int newscore);
+  solve();
+  
+  State currentstate;
+  double temp;
+  const double coolingrate;
+};
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "Course.hpp"
 #include "Classroom.hpp"
 #include "State.hpp"
+#include "Annealing.hpp"
 #include <ctime>
 #include <stdlib.h>
 
@@ -19,9 +20,13 @@ int main(){
   CourseVector courses;
   courses.emplace_back(new Course("AI",2, 7, 15, rooms));
   courses.emplace_back(new Course("TBFO",3, 7, 12, rooms));
+  courses.emplace_back(new Course("OS",4, 7, 12, rooms));
+  courses.emplace_back(new Course("ST",4, 7, 12, rooms));
+  courses.emplace_back(new Course("OOP",4, 7, 12, rooms));
 
   shared_ptr<Classroom> room (new Classroom("7606",8,16));
   Course course("AI",2,7,15, {room});
+  cout << "a" << endl;
   course.set_schedule(Schedule(room,Day::Monday,8,10));
   course.get_schedule();
 
@@ -36,9 +41,16 @@ int main(){
 //  cout << Schedule::intersect(s1,s2) << endl;
   State s(rooms,courses);
   s.init_random_schedule();
-  for (int i=0 ; i<10 ; i++)
-    s = s.mutate();
+  //for (int i=0 ; i<10 ; i++) {
+    //s = s.mutate();
+	//cout << "returned";
+  //}
 	//s.init_random_schedule();
 
+  Annealing a(s,100,0.0003);
+  //for (int i=0 ; i<100 ; i++)
+	  //cout << a.countAcceptanceRate(50,rand()%50) << endl;
+  
+  a.solve();
   return 0;
 }
