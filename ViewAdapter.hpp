12154aaeae42a5fe0dba84a2e1f209fab975e9a0
Isham Faizal <i@adimaja.com>
2016-09-30 15:05:12 +0700

    Initial implementation for ViewAdapter

---
+++ ViewAdapter.hpp
@@ -0,0 +1,42 @@
+#ifndef VIEW_ADAPTER_HPP
+#define VIEW_ADAPTER_HPP
+
+#include <vector>
+#include <sstream>
+#include <string>
+#include <map>
+
+#include "Course.hpp"
+#include "State.hpp"
+#include "Classroom.hpp"
+
+struct CourseTime {
+	int day;
+	int time;
+	int duration;
+	string room_name;
+};
+
+class ViewAdapter {
+public:
+	ViewAdapter();
+
+	void add_room(const string room_description);
+	void add_course(const string course_description);
+
+	void move_course(const string course_name, 
+					 const int src_day, const int src_time,
+					 const int dest_day, const int dest_time);
+
+	CourseTime get_course_result(const string course_name) const;
+
+	void randomize_schedule();
+
+private:
+	std::map<std::string, pCourse> course_by_name;
+	std::map<std::string, pRoom> room_by_name;
+
+	void update_courses(CourseVector& course_results);
+};
+
+#endif // !VIEW_ADAPTER_HPP
