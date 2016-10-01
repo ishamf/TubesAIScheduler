e8c809d82aca5f586139b926b65f4a6d7fc9e304
Isham Faizal <i@adimaja.com>
2016-10-01 18:32:29 +0700

    Added flags that make the build work

--- build_emscripten.bat
+++ build_emscripten.bat
@@ -1,1 +1,1 @@
-em++ --bind -std=c++11 -O2 --post-js post.js ViewAdapter.cpp State.cpp Schedule.cpp Course.cpp Classroom.cpp Annealing.cpp -o out/AI.js
+em++ --bind -std=c++11 -O2 --post-js post.js ViewAdapter.cpp State.cpp Schedule.cpp Course.cpp Classroom.cpp Annealing.cpp -o out/AI.js --memory-init-file 0 -s DISABLE_EXCEPTION_CATCHING=0
